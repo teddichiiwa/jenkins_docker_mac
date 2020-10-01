f44dad748256029b2c6cac86d6c915f5d86dea69
Phan Anh <ptanhvm.ios@gmail.com>
2020-10-01 15:16:52 +0700

    Update

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 # Run Jenkins
-mkdir -p ./jenkins/jenkins_home
-sudo chown -R 1000:1000 ./jenkins/jenkins_home
-docker run -p 8080:8080 -p 50000:50000 -v ./jenkins/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+mkdir -p $PWD/jenkins_home
+docker run -p 8080:8080 -p 50000:50000 -v $PWD/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
 
 # show endpoint
 echo 'Jenkins installed'
